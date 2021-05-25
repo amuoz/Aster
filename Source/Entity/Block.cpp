b3c44b8f4e423c66e189cacfc77401b6db1f7d83
Federico Pérez Fernández <FedeIII@users.noreply.github.com>
2021-05-25 22:58:27 +0200

    Wall top border (#48)
    * wall top textures
    
    * drawn top border for walls
    
    Co-authored-by: FedeIII <federico.perez@coverwallet.com>

--- Source/Entity/Block.cpp
+++ Source/Entity/Block.cpp
@@ -5,27 +5,32 @@
 #include "Physics.h"
 #include "PhysicActor.h"
 
-//	PhysicsActor pos & size
-//	____________________ ^
-//	|									 | |
-//	|		Actor Sprite   | | OFFSET_Y
-//	|								   | |
-//	|			 			_______| v
-//	|			 			|			 |
-//	|		 Physics Actor |
-//	|___________|______|
-//	<----------->
-//		OFFSET_X
+//  PhysicsActor pos & size
+//   __________________  ^
+//  |                  | |
+//  |   Actor Sprite   | | OFFSET_Y
+//  |                  | |
+//  |           _______| v
+//  |           |      |
+//  |    Physics Actor |
+//  |___________|______|
+//  <----------->
+//    OFFSET_X
 
 const float OFFSET_X = 0.66f;
 const float OFFSET_Y = 0.5f;
 
 Block::Block(glm::vec3 pos, glm::vec3 size, std::unique_ptr<Sprite> sprite, glm::vec3 color, BlockLocation location, glm::vec3 velocity) : Actor(pos, size, std::move(sprite), color, velocity)
 {
-	glm::vec3 physicsPosition = GetPhysicsPosition(pos, size, location);
-	glm::vec3 physicsSize = GetPhysicsSize(size, location);
-	ActorCollider = g_PhysicsPtr->AddDynamicActor(physicsPosition, velocity, physicsSize, false, CollisionChannel::STATIC, glm::vec3(0.0f), 1.0f);
-	ActorCollider->report = this;
+	Location = location;
+
+	if (!IsTopLocation())
+	{
+		glm::vec3 physicsPosition = GetPhysicsPosition(pos, size, location);
+		glm::vec3 physicsSize = GetPhysicsSize(size, location);
+		ActorCollider = g_PhysicsPtr->AddDynamicActor(physicsPosition, velocity, physicsSize, false, CollisionChannel::STATIC, glm::vec3(0.0f), 1.0f);
+		ActorCollider->report = this;
+	}
 }
 
 Block::~Block()
@@ -44,11 +49,6 @@ glm::vec3 Block::GetPhysicsPosition(glm::vec3 pos, glm::vec3 size, BlockLocation
 {
 	switch (location)
 	{
-	case BlockLocation::TOP_LEFT:
-		return glm::vec3(pos.x + size.x * OFFSET_X, pos.y + size.y * OFFSET_Y, pos.z);
-	case BlockLocation::TOP:
-	case BlockLocation::TOP_RIGHT:
-		return glm::vec3(pos.x, pos.y + size.y * OFFSET_Y, pos.z);
 	case BlockLocation::LEFT:
 	case BlockLocation::BOTTOM_LEFT:
 		return glm::vec3(pos.x + size.x * OFFSET_X, pos.y, pos.z);
@@ -65,12 +65,9 @@ glm::vec3 Block::GetPhysicsSize(glm::vec3 size, BlockLocation location)
 {
 	switch (location)
 	{
-	case BlockLocation::TOP_LEFT:
-	case BlockLocation::TOP_RIGHT:
 	case BlockLocation::BOTTOM_LEFT:
 	case BlockLocation::BOTTOM_RIGHT:
 		return glm::vec3(size.x * (1.0f - OFFSET_X), size.y * OFFSET_Y, size.z);
-	case BlockLocation::TOP:
 	case BlockLocation::BOTTOM:
 		return glm::vec3(size.x, size.y * OFFSET_Y, size.z);
 	case BlockLocation::LEFT:
@@ -80,4 +77,11 @@ glm::vec3 Block::GetPhysicsSize(glm::vec3 size, BlockLocation location)
 	default:
 		return size;
 	}
+}
+
+bool Block::IsTopLocation()
+{
+	return Location == BlockLocation::TOP ||
+				 Location == BlockLocation::TOP_RIGHT ||
+				 Location == BlockLocation::TOP_LEFT;
 }
