964c0a147e198c6e187c29625c6940ab2dceed7d
Federico Pérez Fernández <FedeIII@users.noreply.github.com>
2021-05-27 23:52:22 +0200

    Unified all power up files (#55)
    * unified all power up files
    
    Co-authored-by: FedeIII <federico.perez@coverwallet.com>

--- Source/Entity/PowerUp.h
+++ Source/Entity/PowerUp.h
@@ -4,18 +4,21 @@
 #include <utility>
 
 #include "Actor.h"
+#include "AsterTypes.h"
 
 enum class AnimationType;
 class Sprite;
 
-class SwordPowerUp : public Actor
+class PowerUp : public Actor
 {
 public:
-	SwordPowerUp(glm::vec2 pos, glm::vec3 size, std::unique_ptr<Sprite> sprite, glm::vec3 color = glm::vec3(1.0f));
-	~SwordPowerUp();
+	PowerUp(PowerUpType type, glm::vec2 pos, glm::vec3 size, std::unique_ptr<Sprite> sprite, glm::vec3 color = glm::vec3(1.0f));
+	~PowerUp();
 
 	void OnBeginOverlapFunction(std::shared_ptr<PhysicActor> other) override;
 	void OnEndOverlapFunction(std::shared_ptr<PhysicActor> other) override;
 
 private:
+	PowerUpType Type;
+
 };
